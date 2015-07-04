aae4fe8d16730965411c3855a6e0e30f8b2d9595
Ankur Handa <handa.ankur@gmail.com>
2015-07-04 10:51:57 +0100

    updates - saving the files
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 
 #include <iostream>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <cvd/image.h>
+#include <cvd/rgb.h>
+#include <cvd/image_ref.h>
+#include <cvd/image_io.h>
 #include "./src/HDLGrabber/custom_hdl_grabber.h"
 
 //#define HAVE_PCAP 1
@@ -58,6 +62,9 @@ int main(void)
 
     bool updated = false;
 
+
+    int count = 0;
+
     while(true)
     {
         CloudConstPtr cloud;
@@ -83,6 +90,98 @@ int main(void)
 
             viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3);
 
+            int height = cloud->height;
+            int width  = cloud->width;
+
+            double pix_res_x = 0.4;
+
+            int widthImg = (int)(360.0/pix_res_x);
+
+            CVD::Image<u_int16_t>LiDARImage(CVD::ImageRef(widthImg,16));
+
+            for(int yy = 0; yy < 16; yy++)
+            {
+                for(int xx = 0; xx < widthImg; xx++)
+                {
+                    LiDARImage[CVD::ImageRef(xx,yy)] = 0;
+                }
+            }
+//            std::cout<<"height = " << height <<", width = " << width << std::endl;
+
+            double dist_max = -1E20;
+
+            std::vector<float>yaws;
+            std::vector<float>pitches;
+
+            char pfileName[100];
+
+            sprintf(pfileName,"points_%06d.txt",count);
+
+            ofstream pointfile(pfileName);
+
+            for(int xx = 0; xx < width*height; xx++)
+            {
+                pcl::PointXYZ p = cloud->at(xx);
+
+                pointfile<<p.x<<", "<<p.y<<", "<<p.z<< std::endl;
+
+                double xr = p.x;
+                double yr = p.y;
+                double zr = p.z;
+
+                double xrs = xr*xr;
+                double yrs = yr*yr;
+                double zrs = zr*zr;
+
+                double dist = sqrt(xrs + yrs + zrs);
+
+                double yawRAD   = atan2(yr,xr) * 180.0f / M_PI; // atan2 returns in [-pi..+pi]
+                double pitchRAD = atan2(-zr,sqrt(xrs+yrs)) * 180.0f / M_PI; // returns in [-pi..+pi]
+
+//                std::cout<<"yawRAD = " << yawRAD <<", pitchRAD = " << pitchRAD << std::endl;
+
+//                printf(" yaw = %f, pitch = %f ", yawRAD, pitchRAD);
+
+                yaws.push_back(yawRAD);
+                pitches.push_back(pitchRAD);
+
+                int hi = (int)(round((yawRAD + 180.0f)/pix_res_x));
+
+                int vi = (int)(round((pitchRAD + 15.0)/2));
+
+                if ( dist_max < dist )
+                    dist_max = dist;
+
+//                if ( pitchRAD && yawRAD )
+                    LiDARImage[CVD::ImageRef(hi,vi)] = (u_int16_t)(dist*5000.0);
+
+            }
+
+            pointfile.close();
+
+            if ( dist_max > 10 )
+                std::cout<<"count = " << count <<" dist_max = " << dist_max << std::endl;
+
+            static char fileName[300];
+
+            sprintf(fileName,"LiDARImage%06d.png",count);
+
+            CVD::img_save(LiDARImage,fileName);
+
+            static char ofileName[300];
+
+            sprintf(ofileName,"yaws_pitches_%06d.txt",count);
+
+            ofstream outfile(ofileName);
+
+            for(int i = 0; i < yaws.size(); i++)
+            {
+                outfile << yaws.at(i)<<", "<<pitches.at(i)<< std::endl;
+            }
+
+            outfile.close();
+
+            count++;
         }
 
         viewer->spinOnce(10);
